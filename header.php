553fe9d368db6ed388f064110bf9812b9701b310
Emily Crook <Emily.A.Crook96@gmail.com>
2022-01-20 11:25:01 -0600

    added login functionality

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -35,15 +38,20 @@
                         <a class="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
-                        <form class="d-flex" action="includes/login.inc.php" method="post">
-                            <input class="form-control me-2" type="text" name="mailuid" placeholder="Username/Email...">
-                            <input class="form-control me-2" name="pwd" placeholder="Password...">
-                            <button class="btn btn btn-dark" type="submit" name="login-submit">Login</button>
-                        </form>
-                        <a class="btn btn-light" href="signup.php">Signup</a>
-                        <form class="d-flex" action="includes/logout.inc.php" method="post">
-                            <button class="btn btn btn-dark" type="submit" name="logout-submit">Logout</button>
-                        </form>        
+                        <?php
+                            if(isset($_SESSION['userId'])) {
+                                echo '<form class="d-flex" action="includes/logout.inc.php" method="post">
+                                    <button class="btn btn btn-dark" type="submit" name="logout-submit">Logout</button>
+                                    </form> ';
+                            } else {
+                                echo '<form class="d-flex" action="includes/login.inc.php" method="post">
+                                    <input class="form-control me-2" type="text" name="mailuid" placeholder="Username/Email...">
+                                    <input class="form-control me-2" type="password" name="pwd" placeholder="Password...">
+                                    <button class="btn btn btn-dark" type="submit" name="login-submit">Login</button>
+                                    </form>
+                                    <a class="btn btn-light" href="signup.php">Signup</a>';
+                            }
+                        ?>       
                     </div>
                 </div>
             </nav>
--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <main>
         <div>
             <div class="status-notification">
-                <p>You are logged out!</p>
-                <p>You are logged in!</p>
+                <?php
+                    if(isset($_SESSION['userId'])) {
+                        echo '<p>You are logged in!</p>';
+                    } else {
+                        echo '<p>You are logged out!</p>';
+                    }
+                ?>
             </div>
         </div>
     </main>
